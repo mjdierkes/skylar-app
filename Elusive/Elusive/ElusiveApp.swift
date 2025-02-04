2cd9d7e50cd76e5c4bd0d0ce2d78392f34bc7ea4
Mason Dierkes <mason@elusive.ai>
2025-02-04 12:47:05 -0800

    reset

---
+++ Elusive/Elusive/ElusiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElusiveApp.swift
+//  Elusive
+//
+//  Created by Mason on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ElusiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ starter/starter/starterApp.swift
@@ -0,0 +1,17 @@
+//
+//  starterApp.swift
+//  starter
+//
+//  Created by Mason on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct starterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
